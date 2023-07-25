59f91907a415dd3ca7fd76d105b8465d12eb3485
darlene503 <duyn503@gmail.com>
2023-07-24 21:55:03 -0700

    edited ex one

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Number guessing game
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+
+            Console.WriteLine(“Try to guess my favorite number”);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+
+                Console.WriteLine($"{userInput} is too low");
+
+            }
+            else if (userInput > favNumber)
+            {
+
+                Console.WriteLine($"{userInput) is PathTooLongException high");
+
+            }
+            else
+            {
+                Console.WriteLine("Nevermind.");
+
+            }           
         }
     }
 }
