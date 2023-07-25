c490b71a371a37d5daa1e561ed847a80bf9b558e
darlene503 <duyn503@gmail.com>
2023-07-24 22:20:37 -0700

    finish ex one

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,15 +2,18 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main(string[]args)
         {
             //Number guessing game
+
             var r = new Random();
-            var favNumber = r.Next(1, 1000);
+            // var favNumber = r.Next(1,1000);
+            var favNumber = 33;
 
-
-            Console.WriteLine(“Try to guess my favorite number”);
+            Console.WriteLine("Try to guess my favorite number");
             var userInput = int.Parse(Console.ReadLine());
+
+
 
             if (userInput < favNumber)
             {
@@ -21,14 +24,20 @@
             else if (userInput > favNumber)
             {
 
-                Console.WriteLine($"{userInput) is PathTooLongException high");
+                Console.WriteLine($"{userInput} is too high");
 
             }
             else
             {
                 Console.WriteLine("Nevermind.");
 
-            }           
+            }
         }
     }
 }
+
+
+
+
+
+
